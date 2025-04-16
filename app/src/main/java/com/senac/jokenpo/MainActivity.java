a933e72ad6f3b0f01ab4f3ea768423b6bf2bdc49
Alisson <alisson.oliveiraferreira23@gmail.com>
2025-04-16 19:59:26 -0300

    Interação com a tela II e criação da tela III

--- app/src/main/java/com/senac/jokenpo/MainActivity.java
+++ app/src/main/java/com/senac/jokenpo/MainActivity.java
@@ -2,13 +2,29 @@ package com.senac.jokenpo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bt_avancar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt_avancar = findViewById(R.id.button1);
+
+        bt_avancar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ir = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(ir);
+            }
+        });
     }
+
 }
